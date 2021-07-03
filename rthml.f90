41c847e092af72a66d0f7fd50f51a38dc309a4b1
LyntonZhou <lingfeng.zhou.cn@gmail.com>
2021-07-03 16:33:54 +0800

    update rthml

--- rthml.f90
+++ rthml.f90
@@ -208,8 +208,8 @@
 
             if (sedrchin > sedrchout) then
                 ! settling
-                setlhml_exch(k)=(sedrchin-sedrchout)/sedrchin*rch_hml_exch(k)
-                setlhml_nlab(k)=(sedrchin-sedrchout)/sedrchin*rch_hml_nlab(k)
+                setlhml_exch(k) = (sedrchin-sedrchout)/sedrchin*rch_hml_exch(k)
+                setlhml_nlab(k) = (sedrchin-sedrchout)/sedrchin*rch_hml_nlab(k)
                 ! check settling veolocity
                 !stl = (sedrchin-sedrchout)/sedrchin*depth/tday
                 !if (stl > 2.5) then ! 10 m/s
@@ -220,8 +220,8 @@
                 !endif
             else
                 ! resuspension
-                rsushml_exch(k)=(sedrchout-sedrchin)/bedmass*sed_hml_exch(k)
-                rsushml_nlab(k)=(sedrchout-sedrchin)/bedmass*sed_hml_nlab(k)
+                rsushml_exch(k) = (sedrchout-sedrchin)/bedmass*sed_hml_exch(k)
+                rsushml_nlab(k) = (sedrchout-sedrchin)/bedmass*sed_hml_nlab(k)
                  ! check resuspension veolocity
                 rsp = (sedrchin-sedrchout)/bedmass*sedhml_act(jrch)/tday
                 if (rsp > 50) then
@@ -337,13 +337,13 @@
 
         end if
         
-        rch_hml_2=rch_hml_sol(k) + rch_hml_exch(k)
-        rch_hml_sol(k)=rch_hml_2 * x1
-        rch_hml_exch(k)=rch_hml_2 * x2
+        rch_hml_2 = rch_hml_sol(k) + rch_hml_exch(k)
+        rch_hml_sol(k) = rch_hml_2 * x1
+        rch_hml_exch(k) = rch_hml_2 * x2
 
-        sed_hml_2=sed_hml_sol(k)  + sed_hml_exch(k)
-        sed_hml_sol(k)=sed_hml_2 * y1
-        sed_hml_exch(k)=sed_hml_2 * y2
+        sed_hml_2 = sed_hml_sol(k)  + sed_hml_exch(k)
+        sed_hml_sol(k) = sed_hml_2 * y1
+        sed_hml_exch(k) = sed_hml_2 * y2
 
         ! calculate h.metal concentrations at end of day
         chhml_conc(:,k,jrch) = 0.
